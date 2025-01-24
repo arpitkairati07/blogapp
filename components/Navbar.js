e63d07f251f1b8b38401e38c7be30db9542bdfaa
arpitkairati2002 <arpitkairati54@gmail.com>
2025-01-24 22:20:54 +0000

    navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -15,10 +15,10 @@ import { ModeToggle } from "./theme-btn";
 
 const Navbar = () => {
   return (
-    <nav className="p-4 sticky top-0 backdrop-blur-lg bg-white">
+    <nav className="p-4 sticky top-0 backdrop-blur-lg bg-transparent">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
-        <div className="text-2xl font-extrabold text-black">
+        <div className="text-2xl font-extrabold text-black dark:text-white">
           <Link href="/">
             BlogVerse - Your Blogging Companion
           </Link>
@@ -28,12 +28,13 @@ const Navbar = () => {
         <Sheet>
           <SheetTrigger asChild>
             <button
-              className="menu-toggle text-black text-2xl md:hidden focus:outline-none"
+              className="menu-toggle text-black text-2xl md:hidden focus:outline-none dark:text-white"
               aria-label="Open Menu"
             >
               ☰
             </button>
           </SheetTrigger>
+          <span className="mx-2 block md:hidden"><ModeToggle></ModeToggle></span>
           <SheetContent side="right" className="bg-white text-black">
             <SheetHeader>
               <SheetTitle className="text-lg font-bold">BlogVerse</SheetTitle>
@@ -67,23 +68,23 @@ const Navbar = () => {
 
         {/* Desktop Menu */}
         <div className="hidden md:flex gap-6 items-center">
-        <Link href="/" className="text-black text-base font-medium hover:text-black hover:text-lg transition-colors">
+        <Link href="/" className="text-black text-base font-medium hover:text-black hover:text-lg hover:font-bold dark:text-white transition-colors">
          Home
         </Link>
 
-        <Link href="/about" className="text-black text-base font-medium hover:text-black hover:text-lg transition-colors">
+        <Link href="/about" className="text-black text-base font-medium hover:text-black hover:text-lg hover:font-bold dark:text-white transition-colors">
          About
         </Link>
-        <Link href="/blog" className="text-black text-base font-medium hover:text-black hover:text-lg transition-colors">
+        <Link href="/blog" className="text-black text-base font-medium hover:text-black hover:text-lg hover:font-bold transition-colors dark:text-white">
          Blog
         </Link>
-          <Link href="/contact" className="text-black text-base font-medium hover:text-black hover:text-lg transition-colors">
+          <Link href="/contact" className="text-black text-base font-medium hover:text-black hover:text-lg hover:font-bold transition-colors dark:text-white">
             Contact
           </Link>
-          <Button variant="outline" className="border-black text-black hover:bg-white hover:text-blue-500 transition-colors">
+          <Button variant="outline" className="border-black dark:border-white text-black dark:text-white hover:text-blue-500 transition-colors">
             Login
           </Button>
-          <Button variant="outline" className="border-black text-black hover:bg-white hover:text-blue-500 transition-colors">
+          <Button variant="outline" className="border-black dark:border-white text-black dark:text-white hover:text-blue-500 transition-colors">
             Signup
           </Button>
           <ModeToggle></ModeToggle>
